99aa892025c539ef2ebdb46f32f95e8343aef7c6
sandunMadhushan <sandunhmadushan9330@gmail.com>
2024-04-17 11:09:37 +0530

    added navigation drawer

--- app/src/main/java/com/s22010304/e_doc/MainActivity2.java
+++ app/src/main/java/com/s22010304/e_doc/MainActivity2.java
@@ -1,8 +1,19 @@
 package com.s22010304.e_doc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +21,57 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        MaterialToolbar toolbar = findViewById(R.id.topAppbar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                item.setChecked(true);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id) {
+                    case R.id.nav_home:
+                        replaceFragment(new HomeFragment());
+                        break;
+                    case R.id.nav_messages:
+                        replaceFragment(new ProfileFragment());
+                        break;
+                    case R.id.nav_trash:
+                        replaceFragment(new AppointmentsFragment());
+                        break;
+                    case R.id.nav_settings:
+                        replaceFragment(new ProfileFragment());
+                        break;
+                    case R.id.nav_login:
+                        replaceFragment(new ProfileFragment());
+                        break;
+                    case R.id.nav_share:
+                        replaceFragment(new ProfileFragment());
+                        break;
+                    case R.id.nav_rate:
+                        replaceFragment(new ProfileFragment());
+                        break;
+                    default:
+                        return true;
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
