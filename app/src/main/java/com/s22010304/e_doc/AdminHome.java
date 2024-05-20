4aa594351c62447c5a45476c7e046332e4cbdad2
sandunMadhushan <sandunhmadushan9330@gmail.com>
2024-05-21 00:45:20 +0530

    fetch registered doctors from db to admin-approve doctors

--- app/src/main/java/com/s22010304/e_doc/AdminHome.java
+++ app/src/main/java/com/s22010304/e_doc/AdminHome.java
@@ -1,21 +1,39 @@
 package com.s22010304.e_doc;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 public class AdminHome extends Fragment {
+
+    LinearLayout approvedoctor;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_admin_home, container, false);
+
+
+        approvedoctor=view.findViewById(R.id.approvedoctor);
+
+        approvedoctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ApproveDoctor.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
