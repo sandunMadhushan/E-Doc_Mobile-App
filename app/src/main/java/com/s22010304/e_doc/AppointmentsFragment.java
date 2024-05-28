6b7424eae1766753b66ed832785936e8de5fe661
sandunMadhushan <sandunhmadushan9330@gmail.com>
2024-05-28 09:00:33 +0530

    get appointment details from user selections

--- app/src/main/java/com/s22010304/e_doc/AppointmentsFragment.java
+++ app/src/main/java/com/s22010304/e_doc/AppointmentsFragment.java
@@ -1,41 +1,36 @@
 package com.s22010304.e_doc;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.s22010304.e_doc.databinding.FragmentAppointmentsBinding;
 
-
 public class AppointmentsFragment extends Fragment {
-    FragmentAppointmentsBinding binding;
+    private FragmentAppointmentsBinding binding;
 
-    public AppointmentsFragment() {
-
-    }
-
-
+    public AppointmentsFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentAppointmentsBinding.inflate(inflater,container,false);
+        binding = FragmentAppointmentsBinding.inflate(inflater, container, false);
         binding.detailsbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BookAppointmentFragment  BookAppointmentFragment = new BookAppointmentFragment();
-                FragmentTransaction transaction = getFragmentManager().beginTransaction();
-                transaction.replace(R.id.appointmentsfragment,BookAppointmentFragment);
-                transaction.commit();
+                loadBookAppointmentFragment();
             }
         });
-
         return binding.getRoot();
     }
 
+    private void loadBookAppointmentFragment() {
+        BookAppointmentFragment bookAppointmentFragment = new BookAppointmentFragment();
+        FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.appointmentsfragment, bookAppointmentFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
